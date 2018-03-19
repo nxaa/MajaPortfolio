76400649f2eec8a2620620744e6111ac1a46b4e3
Mateusz <mateusz.bodurkiewicz@nexio.pl>
2018-03-19 20:49:14 +0100

    Layout on both - large and small screen + thumbnails

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject, NgModule, VERSION } from '@angular/core';
 import { Http } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import {Observable} from 'rxjs';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/filter';
 
 import {
     AccessibilityConfig, Action, AdvancedLayout, ButtonEvent, ButtonsConfig, ButtonsStrategy, ButtonType, Description, DescriptionStrategy,
@@ -14,9 +19,42 @@ import {
 })
 export class GalleryComponent {
 
+  oldWidth = document.documentElement.clientWidth;
+  width = document.documentElement.clientWidth;
+  constructor() {
+    const $resizeEvent = Observable.fromEvent(window, 'resize')
+      .map(() => {
+        return document.documentElement.clientWidth;
+        })
+      .debounceTime(200)
+
+
+      $resizeEvent.subscribe(data => {
+        this.width = data;
+        // sounds good, doesnt work
+        this.updateGalleryLayout();
+      });
+  }
+
+  updateGalleryLayout() {
+    if((this.width > 768 && this.oldWidth < 768) || (this.width < 768 && this.oldWidth > 768)) {
+      this.plainGalleryGrid.layout = this.getGalleryLayout();
+      this.oldWidth = this.width;
+    }
+  }
+
+  getGalleryLayout() {
+    if(this.width < 768) {
+      return new GridLayout({ width: '45%', height: '30%' }, { length: 2, wrap: true });
+    }
+
+    return new GridLayout({ width: '20%', height: '5%' }, { length: 4, wrap: true });
+  }
+
+
     plainGalleryGrid: PlainGalleryConfig = {
       strategy: PlainGalleryStrategy.GRID,
-      layout: new GridLayout({ width: '20%', height: '5%' }, { length: 4, wrap: true })
+      layout: this.getGalleryLayout()
       
     };
 
@@ -26,6 +64,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/frozen1.jpg',
             description: 'Frozen'
+          },
+          {
+            img: 'assets/gallery/thumb/frozen1.jpg'
           }
         ),
         new Image(
@@ -33,6 +74,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/frozen2.jpg',
             description: 'Frozen'
+          },
+          {
+            img: 'assets/gallery/thumb/frozen2.jpg'
           }
         ),
         new Image(
@@ -40,6 +84,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/frozen3.jpg',
             description: 'Frozen'
+          },
+          {
+            img: 'assets/gallery/thumb/frozen3.jpg'
           }
         ),
         new Image(
@@ -47,6 +94,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/Edge.jpg',
             description: 'Edge'
+          },
+          {
+            img: 'assets/gallery/thumb/Edge.jpg'
           }
         ),
         new Image(
@@ -54,6 +104,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/LaDolceVita.jpg',
             description: 'La Dolce Vita'
+          },
+          {
+            img: 'assets/gallery/thumb/LaDolceVita.jpg'
           }
         ),
         new Image(
@@ -61,6 +114,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/LaDolceVita-zdjecia.jpg',
             description: 'La Dolce Vita'
+          },
+          {
+            img: 'assets/gallery/thumb/LaDolceVita-zdjecia.jpg'
           }
         ),
         new Image(
@@ -68,6 +124,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/tropicalgirl1.jpg',
             description: 'Tropical girl'
+          },
+          {
+            img: 'assets/gallery/thumb/tropicalgirl1.jpg'
           }
         ),
         new Image(
@@ -75,6 +134,9 @@ export class GalleryComponent {
           { // modal
             img: 'assets/gallery/tropicalgirl2.jpg',
             description: 'Tropical girl'
+          },
+          {
+            img: 'assets/gallery/thumb/tropicalgirl2.jpg'
           }
         )
       ];
